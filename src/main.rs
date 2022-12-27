39f770f3d134d8ab9729e5fdcf2bf8ea18c03db6
Matas Minelga <minematas@gmail.com>
2022-12-28 00:51:26 +0200

    23 prongrens

--- src/main.rs
+++ src/main.rs
@@ -824,6 +824,172 @@ mod day09 {
         print!("Day 9: {}\n", tail_nodes_visited.len() + 1);
     }
 }
+
+mod day23 {
+    const GRID_SIZE : usize = 10;
+
+    pub fn construct_grid_from_positions(elve_positions: &Vec<(usize, usize)>) -> [[char; GRID_SIZE]; GRID_SIZE] {
+        let mut grid: [[char; GRID_SIZE]; GRID_SIZE] = [['.'; GRID_SIZE]; GRID_SIZE];
+        for elve_position in elve_positions {
+            let (x, y) = elve_position;
+            grid[*x][*y] = '#';
+        }
+        grid
+    }
+
+    pub fn check_direction(grid: &[[char; GRID_SIZE]; GRID_SIZE], x: usize, y: usize, direction: char) -> bool {
+        match direction {
+            'N' => {
+                grid[x-1][y-1] != '#' && grid[x-1][y] != '#' && grid[x-1][y+1] != '#'
+            }
+            'S' => {
+                grid[x+1][y-1] != '#' && grid[x+1][y] != '#' && grid[x+1][y+1] != '#'
+            }
+            'W' => {
+                grid[x-1][y-1] != '#' && grid[x][y-1] != '#' && grid[x+1][y-1] != '#'
+            }
+            'E' => {
+                grid[x-1][y+1] != '#' && grid[x][y+1] != '#' && grid[x+1][y+1] != '#'
+            }
+            _ => panic!("Unknown direction"),
+        }
+    }
+
+    pub fn do_round(elve_positions: &mut Vec<(usize, usize)>, grid: &mut [[char; GRID_SIZE]; GRID_SIZE]) -> [[char; GRID_SIZE]; GRID_SIZE] {
+        let mut next_positions: Vec<(usize, usize)> = elve_positions.clone();
+
+        let mut next_grid: [[i32; GRID_SIZE]; GRID_SIZE] = [[0; GRID_SIZE]; GRID_SIZE];
+
+        for (i, elve_position) in elve_positions.iter().enumerate() {
+            let (x, y) = elve_position;
+            // checking 8 directions
+            let mut found_neighbour = false;
+            for xx in -1..2 {
+                for yy in -1..2 {
+                    if xx == 0 && yy == 0 {
+                        continue;
+                    }
+                    if grid[(*x as i32 + xx) as usize][(*y as i32 + yy) as usize] == '#' {
+                        found_neighbour = true;
+                        break;
+                    }
+                }
+            }
+            if !found_neighbour {
+                continue;
+            }
+
+            // Checking North
+            if grid[*x-1][*y-1] != '#' && grid[*x-1][*y] != '#' && grid[*x-1][*y+1] != '#' { 
+                next_positions[i].0 -= 1;
+                next_grid[*x-1][*y] += 1;
+                continue;
+            }
+            // Checking South
+            if grid[*x+1][*y-1] != '#' && grid[*x+1][*y] != '#' && grid[*x+1][*y+1] != '#' { 
+                next_positions[i].0 += 1;
+                next_grid[*x+1][*y] += 1;
+                continue;
+            }
+            // Checking West
+            if grid[*x-1][*y-1] != '#' && grid[*x][*y-1] != '#' && grid[*x+1][*y-1] != '#' { 
+                next_positions[i].1 -= 1;
+                next_grid[*x][*y-1] += 1;
+                continue;
+            }
+            // Checking East
+            if grid[*x-1][*y+1] != '#' && grid[*x][*y+1] != '#' && grid[*x+1][*y+1] != '#' { 
+                next_positions[i].1 += 1;
+                next_grid[*x][*y+1] += 1;
+                continue;
+            }
+            
+        }
+        for i in 0..elve_positions.len() {
+            if next_grid[next_positions[i].0][next_positions[i].1] > 1 {
+                next_positions[i] = elve_positions[i];
+            }
+        }
+        for i in 0..next_positions.len() {
+            elve_positions[i] = next_positions[i];
+        }
+        let new_grid: [[char; GRID_SIZE]; GRID_SIZE] = construct_grid_from_positions(&next_positions);
+        new_grid      
+
+
+
+    }
+
+
+    pub fn get_grid_bbox(elve_positions: &Vec<(usize, usize)>) -> (usize, usize, usize, usize) {
+        let mut min_x = GRID_SIZE;
+        let mut max_x = 0;
+        let mut min_y = GRID_SIZE;
+        let mut max_y = 0;
+        for (x, y) in elve_positions {
+            if *x < min_x {
+                min_x = *x;
+            }
+            if *x > max_x {
+                max_x = *x;
+            }
+            if *y < min_y {
+                min_y = *y;
+            }
+            if *y > max_y {
+                max_y = *y;
+            }
+        }
+        (min_x, max_x, min_y, max_y)
+    }
+
+    pub fn count_empty_spaces_in_bbox(elve_positions: &Vec<(usize, usize)>, min_x: usize, max_x: usize, min_y: usize, max_y: usize) -> usize {
+        let count = (max_x - min_x) * (max_y - min_y) - elve_positions.len();
+        count
+    }
+
+    pub fn center_positions(elve_positions: &Vec<(usize, usize)>, grid: &mut [[char; GRID_SIZE]; GRID_SIZE]) -> Vec<(usize, usize)> {
+        let (min_x, max_x, min_y, max_y) = get_grid_bbox(&elve_positions);
+        let mut new_positions: Vec<(usize, usize)> = Vec::new();
+        let mut new_grid: [[char; GRID_SIZE]; GRID_SIZE] = [['.'; GRID_SIZE]; GRID_SIZE];
+        let offset_x:i32 = ((GRID_SIZE - max_x + min_x) / 2) as i32 - min_x as i32;
+        let offset_y:i32 = ((GRID_SIZE - max_y + min_y) / 2) as i32 - min_y as i32;
+        for (x, y) in elve_positions {
+            let new_x = (*x  as i32 + offset_x) as usize;
+            let new_y = (*y  as i32 + offset_y) as usize;
+            new_positions.push((new_x, new_y));
+            new_grid[new_x][new_y] = '#';
+        }
+        *grid = new_grid;
+        new_positions
+    }
+
+    pub fn game_of_elves(data: String) {
+        let n_rounds: usize = 10;
+        let mut grid: [[char; GRID_SIZE]; GRID_SIZE] = [['.'; GRID_SIZE]; GRID_SIZE];
+        let mut elve_positions: Vec<(usize, usize)> = Vec::new();
+        
+        let lines = data.lines();
+        for (i, line) in lines.enumerate() {
+            for (j, c) in line.chars().enumerate() {
+                if c == '#' {
+                    elve_positions.push((i, j));
+                    grid[i][j] = '#';
+                }
+            }
+        }
+
+
+        for _round in 0..n_rounds {
+
+            elve_positions = center_positions(&elve_positions, &mut grid);
+            grid = do_round(&mut elve_positions, &mut grid);
+        }
+        let (min_x, max_x, min_y, max_y) = get_grid_bbox(&elve_positions);
+        let empty_spaces = count_empty_spaces_in_bbox(&elve_positions, min_x, max_x, min_y, max_y);
+        print!("Day 23: {}\n", empty_spaces);
+    }
+}
 fn main() {
     day01::find_max(read_input(1));
     day02::calculate_scores(read_input(2));
@@ -838,6 +1004,7 @@ fn main() {
     day08::get_most_scenic_tree(read_input(8));
     day09::calculate_tail_moves(read_input(9));
     day09::calculate_multiple_knots_moves(read_input(9));
+    day23::game_of_elves(read_input(23));
 }
 
 fn read_input(day: usize) -> String {
